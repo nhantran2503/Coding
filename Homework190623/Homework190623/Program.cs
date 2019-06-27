be9f5f5e832b741633faa089dbcec03c9271a68b
Osiris121 <trandinhnhan121@gmail.com>
2019-06-27 17:24:38 +0700

    bai tap ve nha 190623

--- Homework190623/Homework190623/Program.cs
+++ Homework190623/Homework190623/Program.cs
@@ -14,7 +14,9 @@ namespace Homework190623
             isletterCount(str);
             wordCount(str);
 
-            string num = userInput("formular");
+            string num = "";
+            calculator(num);
+
 
             Console.ReadLine();
         }
@@ -69,6 +71,102 @@ namespace Homework190623
             }
             string result = string.Join(" ", word);
             Console.WriteLine("The contained words are: " + result);
+        }
+
+        static void calculator(string str)
+        {
+            double result;
+            bool check;
+            do
+            {
+                str = userInput("formular");
+                check = inputCheck(str);
+
+            } while (check != true);
+            string[] part = stringSeparate(str);
+            switchCase(str, double.Parse(part[0]), double.Parse(part[1]), out result);
+            Console.WriteLine("The result is: {0}", result);
+            Console.WriteLine("Thank you, have a nice day!!");
+        }
+
+        static bool inputCheck(string str)
+        {
+            string[] part = stringSeparate(str);
+            int count = 0;
+            
+            for (int i = 0; i < part.Length; i++)
+            {
+                if (isNumber(part[i]))
+                    count++;
+            }
+            if (count < 2)
+            {
+                Console.WriteLine("Please input again");
+                return false;
+            }
+            else
+                return true;
+        }
+
+        static string[] stringSeparate(string str)
+        {
+            string[] separator = new string[] { "+", "-", "*", "/", " ", "," };
+            return str.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        static void switchCase(string str, double a, double b, out double c)
+        {
+            c = 0;
+            if (str.Contains("+"))
+                c = add(a, b);
+            else if (str.Contains("-"))
+                c = minus(a, b);
+            else if (str.Contains("*"))
+                c = multiply(a, b);
+            else if (str.Contains("/"))
+                divide(a, b, out c);
+            else
+                Console.WriteLine("Invalid calculator");
+        }
+
+        static bool isNumber(string str)
+        {
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (char.IsNumber(str[i])==false)
+                    return false;
+            }
+            return true;
+        }
+
+        static double add(double a, double b)
+        {
+            return a + b;
+        }
+
+        static double minus(double a, double b)
+        {
+            return a - b;
+        }
+
+        static double multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        static bool divide(double a, double b, out double c)
+        {
+            if (b == 0)
+            {
+                c = 0;
+                Console.WriteLine("Invalid value");
+                return false;
+            }
+            else
+            {
+                c = a / b;
+                return true;
+            }
         }
     }
 }
